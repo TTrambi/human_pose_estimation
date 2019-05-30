e712445f98affc511b18f725ed0a8eedf74de5d5
Thomas Tramberger <thomas.tramberger@gmail.com>
2019-05-30 15:34:49 +0200

    included argpares for python module

--- src/human_pose_estimation.py
+++ src/human_pose_estimation.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 
-
+import os
 import sys
 sys.path = ['/home/pi/Downloads/inference_engine_vpu_arm/python/python2.7'] + sys.path
 import cv2
-
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from pose_estimation_msgs.msg import *
-
 import human_pose_estimation_functions
+
 import numpy as np
 import time
-import human_pose_estimation_functions
-
 import argparse
 
 ############################################
 ############ HANDLE ARGS ###################
 ############################################
-parser = argparse.ArgumentParser()
-parser.add_argument('--network', type=int, help='Select Network for Human Pose Estimation, 1=fastNN, 2=openPoseSingle, 3=openPoseMulti')
+parser = argparse.ArgumentParser(description='Ros publisher node for Human pose estimation')
+parser.add_argument('-m', '--model', type=str, help='Path to the network models .xml file, .bin has to have same name in the same directory')
+parser.add_argument('-o', '--output', type=int, default=0, help='Choose Output Model, 0=COCO(default), 1=MPII, 2=Reduced')
+parser.add_argument('-i', '--input', type=str, help='Ros publisher note to get image from, otherwise opencv is used (with gstreamer over updsrc with prot 3000)' )
+parser.add_argument('-vis', '--visualize', action='store_true', help='visualize the pose estimation in cv window')
+args = parser.parse_args()
 
 
 
@@ -29,42 +30,44 @@ parser.add_argument('--network', type=int, help='Select Network for Human Pose E
 ######### DEFINE POSE POINTS ###############
 ############################################
 #https://www.learnopencv.com/deep-learning-based-human-pose-estimation-using-opencv-cpp-python/
-#COCO Output Format
-#BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-#               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-#               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-#               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
 
-
-#POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-#               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-#               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-#               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-#               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
-
-
+#set output 
+if args.output == 1:
 #MPII Output Format
-#BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-#               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-#               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14,
-#               "Background": 15 }
+	BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+				"LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+				"RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14,
+				"Background": 15 }
 
-#POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-#               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-#               ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Chest", "LHip"],
-#               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Head"] ]
-
+	POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+				["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+				["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Chest", "LHip"],
+				["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Head"] ]
+elif args.output == 2:
 ##for fast eva                  
-BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13}
+	BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+				"LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+				"RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13}
 
-POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Head"] ]
+	POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+				["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+				["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+				["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Head"] ]
+else:
+	#COCO Output Format
+	BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+				"LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+				"RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+				"LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
 
-           
+
+	POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+				["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+				["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+				["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+				["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+   
 # COCO Output Format
 keypointsMapping = ['Nose', 'Neck', 'R-Sho', 'R-Elb', 'R-Wr', 'L-Sho', 'L-Elb', 'L-Wr', 'R-Hip', 'R-Knee', 'R-Ank', 'L-Hip', 'L-Knee', 'L-Ank', 'R-Eye', 'L-Eye', 'R-Ear', 'L-Ear']
 
@@ -90,22 +93,42 @@ colors = [ [0,100,255], [0,100,255], [0,255,255], [0,100,255], [0,255,255], [0,1
 ############################################
 ############# INIT NETWORK #################
 ############################################
+modelXMLPath = ""
+modelBinPath = ""
 #Load pose estimation model
-net = cv2.dnn.readNet('/home/pi/catkin_ws/src/pose_estimation/src/networks/human-pose-estimation-0001.xml','/home/pi/catkin_ws/src/pose_estimation/src/networks/human-pose-estimation-0001.bin')
-#net = cv2.dnn.readNet('/home/pi/catkin_ws/src/pose_estimation/src/networks/model.xml','/home/pi/catkin_ws/src/pose_estimation/src/networks/model.bin')
+if args.model == None:
+	filepath = os.path.dirname(os.path.realpath(__file__))
+	modelXMLPath = filepath + "/networks/human-pose-estimation-0001.xml"
+	modelBinPath = filepath + "/networks/human-pose-estimation-0001.bin"
+else:
+	modelXMLPath = args.model
+	if os.path.isfile(modelXMLPath) == False:
+		print("Could not find model file (.xml)")
+		sys.exit(0)
+	modelBinPath = modelXMLPath[:-4] + ".bin"
+	if os.path.isfile(modelBinPath) == False:
+		print("Could not find model file (.bin)")
+		sys.exit(0)
+	
+try:
+	#read network via opencv
+	net = cv2.dnn.readNet(modelXMLPath,modelBinPath)
 
-#Set device to Neural Compute Stick
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
-
-#init CV Bridge 
-bridge = CvBridge()
-
+	#Set device to Neural Compute Stick
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+	
+	#init CV Bridge 
+	bridge = CvBridge()
+except:
+	print("Could not initialize the network or cvBridge!")
+	sys.exit(0)
+		
 #Create Ros publisher
 pub = rospy.Publisher('/pose_estimation', Persons, queue_size=1)
 
 
 ############################################
-############# INIT NETWORK #################
+##### Processing Human Pose Functions ######
 ############################################
 
 #Process network output
@@ -148,22 +171,23 @@ def processPoseSingle(frame, output):
 	pub.publish(msgPersons)
 
 	#Show body parts in image
-	for pair in POSE_PAIRS:
-		partFrom = pair[0]
-		partTo = pair[1]
-		assert(partFrom in BODY_PARTS)
-		assert(partTo in BODY_PARTS)
+	if args.visualize:
+		for pair in POSE_PAIRS:
+			partFrom = pair[0]
+			partTo = pair[1]
+			assert(partFrom in BODY_PARTS)
+			assert(partTo in BODY_PARTS)
 
-		idFrom = BODY_PARTS[partFrom]
-		idTo = BODY_PARTS[partTo]
+			idFrom = BODY_PARTS[partFrom]
+			idTo = BODY_PARTS[partTo]
 
-		if points[idFrom] and points[idTo]:
-			cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-			cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
-			cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+			if points[idFrom] and points[idTo]:
+				cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+				cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+				cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
 
-	cv2.imshow('image',frame)
-	cv2.waitKey(1)
+		cv2.imshow('image',frame)
+		cv2.waitKey(1)
 
 def processPoseMulti(frame, output):
 	#MULTI PERSON 
@@ -233,17 +257,14 @@ def processPoseMulti(frame, output):
 	cv2.waitKey(1)
 		
 
+
+############################################
+########### Essential Fuinction ############
+############################################
 def callback(data):
-	#print("Message received")
 	frame = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 
-	#cv2.imshow('Feed', frame)
-	#cv2.waitKey(1)
-	print("Test")
 	#feed neural network and process
-	#size for openpose network
-	#blob = cv2.dnn.blobFromImage(frame, size=(456, 256), ddepth=cv2.CV_8U)
-	#size for fast network
 	blob = cv2.dnn.blobFromImage(frame, size=(192, 192), ddepth=cv2.CV_8U)
 	net.setInput(blob)
 
@@ -253,66 +274,51 @@ def callback(data):
 	#multi-person pose estimation with openpose network
 	#names = ['Mconv7_stage2_L1','Mconv7_stage2_L2']
 	#out = net.forward(names)
-
-	
 	#output = np.zeros((1,out[0].shape[1]+out[1].shape[1],out[0].shape[2], out[0].shape[3]))
-
 	#for i  in range(out[1].shape[1]):
 	#	output[:,i,:,:] = out[1][:,i,:,:]
-	
 	#for i  in range(out[0].shape[1]):
 	#	output[:,i+out[1].shape[1],:,:] = out[0][:,i,:,:]
 	
+	
 	#extract the pose from the neural network output, single or multi person pose estimation
 	processPoseSingle(frame, out)
-	#processPoseMulti(frame, output)
+
 
 def main():
 	print('Started listening')
 	rospy.init_node('pose_estimation')
-	#sub = rospy.Subscriber("/camera/image_raw", Image, callback)
-	cap = cv2.VideoCapture('updsrc port=3000 ! application/x-rtp, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! appsink', cv2.CAP_GSTREAMER)
-	#i guess this command does not work :(, buffer is still 5 
-	cap.set(cv2.CAP_PROP_BUFFERSIZE, 0);
-	if not cap.isOpened():
-		print('VideoCapture not opened')
-		exit(0)
-
-	print('VideoCapture opened')
-
-	while True:
-		startTime = time.time()
-		#default buffer size is 5, so empty buffer every time
-		for i in xrange(1):
-			cap.grab()
+	if args.input != None:
+		print("Subscribing to %s", args.input)
+		sub = rospy.Subscriber(args.input, Image, callback)
+	else:
+		print("Using opencv and gstreamer, udpsrc port=3000")
+		cap = cv2.VideoCapture('udpsrc port=3000 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+		#i guess this command does not work :(, buffer is still 5 
+		#cap.set(cv2.CAP_PROP_BUFFERSIZE, 0);
+		if not cap.isOpened():
+			print('OpenCV VideoCapture not opened')
+			exit(0)
+		print('OpenCV VideoCapture opened')
+		while True:
+			#default buffer size is 5, so empty buffer every time
+			for i in xrange(1):
+				cap.grab()
 			
-		ret,frame = cap.read()
+			ret,frame = cap.read()
 		
-		if not ret:
-			print('empty frame')
-			break
-
-		#cv2.imshow('receive', frame)
-		#if cv2.waitKey(1)&0xFF == ord('q'):
-		#	break
-			
-		#startTime = time.time()
+			if not ret:
+				print('empty frame')
+				break
 		
-		#blob = cv2.dnn.blobFromImage(frame, size=(192, 192), ddepth=cv2.CV_8U)
-		blob = cv2.dnn.blobFromImage(frame, size=(456, 256), ddepth=cv2.CV_8U)
-		net.setInput(blob)
-		out = net.forward()
-		#midTime = time.time()
+			blob = cv2.dnn.blobFromImage(frame, size=(192, 192), ddepth=cv2.CV_8U)
+			#blob = cv2.dnn.blobFromImage(frame, size=(456, 256), ddepth=cv2.CV_8U)
+			net.setInput(blob)
+			out = net.forward()
 		
-		#processPoseSingle(frame, out)
-		endTime = time.time()
+			processPoseSingle(frame, out)
+		cap.release()
 		
-		#print(midTime - startTime)
-		print(endTime - startTime)
-		print('\n')
-		
-		
-	cap.release()
 	rospy.spin()
 
 
